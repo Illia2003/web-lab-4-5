a32da880d0b8860bdd771c35e2d4f189773f6e03
Illia2003 <ilia.gumeniuk2018@gmail.com>
2023-11-09 10:54:00 +0200

    Код лабораторної

---
+++ DbConnection/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using LR4.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LR4.DbConnection;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> opts) : base(opts) { }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Size> Size { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>()
+            .HasMany(t => t.Sizes)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ Entities/Size.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LR4.Entities;
+
+public class Size : BaseEntity
+{
+    [Required(ErrorMessage = "Поле обов'язкове для введення")]
+    public string SizeSybmol { get; set; }
+
+    [Required(ErrorMessage = "Поле обов'язкове для введення")]
+    public double Price { get; set; }
+
+    public Guid ProductId { get; set; }
+}
